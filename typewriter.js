a8f57689bc103932d183c8f044347351136c1e58
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-08-25 20:30:36 +0000

    added set timeout

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,7 @@
 const sentence = "every good boy does fine";
 
 for( const char of sentence) {
-  process.stdout.write(char);
-}
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 1000)
+};
